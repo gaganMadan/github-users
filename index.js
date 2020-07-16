f84e8683b4c6c949929b0c8bc7394bf49204ef09
gaganMadan <gagan7535@gmail.com>
2020-07-16 21:55:19 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from '/pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+//import { GithubProvider } from './context/context';
+//import { Auth0Provider } from '@auth0/auth0-react';
 
-function App() {
-  return (
-    <div>
-       <Dashboard></Dashboard>
-      // <Login />
-      // <Error />
-    </div>
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-export default App;
-
-render(<App />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
